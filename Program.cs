ee97fa45b67680b244df1a8ca101859f522c2891
pedrongomes27 <pedro.ngomes27@hotmail.com>
2021-06-25 10:30:06 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordenando_inteiros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                num.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            num.Sort();
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
